44eb7929432493b22e4a3e67e36d25468d7b95f4
Kirankrishna213 <kkirankrishnar@gmail.com>
2025-05-09 22:25:24 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* Base Styles */
+:root {
+  --gold: #D4AF37;
+  --dark-gold: #996515;
+  --light-gold: #F0E68C;
+  --bg-color: #f9f5f0;
+  --text-color: #333;
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 
-// If you want to measure performance
-reportWebVitals(console.log);
+body {
+  font-family: 'Playfair Display', serif;
+  background-color: var(--bg-color);
+  color: var(--text-color);
+  line-height: 1.6;
+}
+
+/* Loading States */
+.loading-screen {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+}
+
+.spinner {
+  border: 5
